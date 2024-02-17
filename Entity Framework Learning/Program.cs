0c7750c75255325a350991601e2efc4449ddc19b
John van Dijk <fortyfour-three@teksavvy.com>
2024-02-17 05:57:31 -0500

    Add project files.

---
+++ Entity Framework Learning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity_Framework_Learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
