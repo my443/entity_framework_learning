50285b1391a115ff70c156bcfa2391952493d717
John van Dijk <fortyfour-three@teksavvy.com>
2024-02-18 05:53:12 -0500

    Beginning of the day.

--- Entity Framework Learning/Program.cs
+++ Entity Framework Learning/Program.cs
@@ -7,8 +7,11 @@ namespace Entity_Framework_Learning
         static void Main(string[] args)
         {
             //addUser();
+            //string csvFileName = "C:\\Users\\jvand\\source\\repos\\Entity Framework Learning\\input.csv";
+            string csvFileName = @"C:\Projects\2024-02-11 T3010 Project\T3010_Data_Files\schedule_2_goods_2021.csv";
+
             ReadCSV readCSV = new ReadCSV();
-            DataTable dataTable =  readCSV.GetDataTabletFromCSVFile("C:\\Users\\jvand\\source\\repos\\Entity Framework Learning\\input.csv");
+            DataTable dataTable =  readCSV.GetDataTabletFromCSVFile(csvFileName);
 
             Console.WriteLine("Success!");
         }
--- Entity Framework Learning/ReadCSV.cs
+++ Entity Framework Learning/ReadCSV.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Csv;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -18,8 +19,17 @@ namespace Entity_Framework_Learning
         public DataTable GetDataTabletFromCSVFile(string csv_file_path) { 
             DataTable csvData = new DataTable();
             
-            string csv = File.ReadAllText(csv_file_path);
-            Console.WriteLine(csv);
+            string csv = File.ReadAllText(csv_file_path, Encoding.Latin1);
+
+            ICsvLine[] lines = CsvReader.ReadFromText(csv).ToArray();
+            var headers = lines[0].Headers;
+
+            //Console.WriteLine(csv);
+            Console.WriteLine(lines[0]);
+
+            foreach (var header in headers) { 
+                Console.WriteLine(header);
+            }
 
             return csvData;
         
